c9db1c78746b5ef5455c74e07b883678b53b27da
adeolaseunaderoju <adeolaseunaderoju@gmail.com>
2022-11-29 16:18:04 -0800

    Task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-lastDigit =0
+lastDigit = 0
 
 if number >= 0:
     lastDigit = number % 10
@@ -16,3 +16,4 @@ elif lastDigit > 5 and lastDigit % 10 != 0:
     print(f"{message} and is greater than 5")
 else:
     print(f"{message} and is less than 6 and not 0")
+
